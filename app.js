f2cbcd8a1cb002ce23c960df3110001b5c50fa93
Zhiquan Zhang <zzq20y@gmail.com>
2020-04-18 18:00:31 -0400

    The homepage router init

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ var Koa = require('koa');
 var Router = require('koa-router');
 var app = new Koa();
 var router = new Router();
+var views = require('koa-views');
 
-router.get('/', (ctx) => {
-    ctx.body = 'Hello!';
+app.use(views('views', {
+    extension:'ejs'
+}))
+
+router.get('/', async (ctx) => {
+    await ctx.render('home');
 })
 
 app.use(router.routes());
