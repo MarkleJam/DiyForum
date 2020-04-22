0c4dfaa934e9ab75bd73e821157ac03e58bca091
Zhiquan Zhang <zzq20y@gmail.com>
2020-04-21 20:52:41 -0400

    Support edit and delete comment

--- routes/comment.js
+++ routes/comment.js
@@ -5,6 +5,8 @@ const UserCollection = 'user';
 const CommentCollection = 'comment';
 const ArticalCollection = 'article';
 
+
+//Below is for adding new comment
 router.get('/new',isLoggedIn, async (ctx) => {
     //console.log(ctx.params.id);
     let article = await DB.find(ArticalCollection, {'_id':DB.getObjectID(ctx.params.id)});
@@ -12,17 +14,67 @@ router.get('/new',isLoggedIn, async (ctx) => {
     //await ctx.render('addArticle');
 })
 
-router.post('/', async (ctx)=>{
+router.post('/doAdd', async (ctx)=>{
     let articles = await DB.find(ArticalCollection, {'_id':DB.getObjectID(ctx.params.id)});
+    
+    let newAuthor = {id: ctx.req.user._id, username:ctx.req.user.username}    
     let newComment = ctx.request.body;
+    newComment.author = newAuthor;
     
     article = articles[0];
     article.comments.push(newComment);
-
+    console.log('Comments are: ' + JSON.stringify(article.comments));    
+    console.log('Type of the comment is:' + (JSON.stringify(article.comments[0])));
+    
     DB.insert(CommentCollection, newComment);
     DB.update(ArticalCollection, {'_id':DB.getObjectID(article._id)}, article);
 
     ctx.redirect('/article/show?id=' + article._id);
+})
+
+//Below is for editting comment
+router.get('/:commentid/edit', async (ctx) => {
+    //console.log(ctx.params.id);
+    let articles = await DB.find(ArticalCollection, {'_id':DB.getObjectID(ctx.params.id)});
+    article = articles[0];
+    let comments = await DB.find(CommentCollection, {'_id':DB.getObjectID(ctx.params.commentid)});
+    let comment = comments[0];    
+    await ctx.render('comments/edit',{Comment: comment, Article: article});
+})
+
+router.post('/:commentid/doEdit', async (ctx) => {    
+    let newComment = ctx.request.body;
+    let author = {id: ctx.req.user._id, username:ctx.req.user.username}    
+    newComment.author = author;
+    
+    let articles = await DB.find(ArticalCollection, {'_id':DB.getObjectID(ctx.params.id)});
+    let article = articles[0];
+
+    article.comments.filter(comment => comment._id ==  ctx.params.commentid).map(x =>(x.CommentCont = newComment.CommentCont)); 
+
+    await DB.update(ArticalCollection, {'_id':DB.getObjectID(ctx.params.id)}, {'comments':article.comments});
+    await DB.update(CommentCollection, {'_id':DB.getObjectID(ctx.params.commentid)}, newComment);
+    
+    return ctx.redirect('/article/show?id=' + ctx.params.id);
+})
+
+router.get('/:commentid/doDelete',async (ctx) => {
+    let id = ctx.params.commentid;
+    var result = await DB.delete(CommentCollection, {'_id':DB.getObjectID(id)});
+    
+    let articles = await DB.find(ArticalCollection, {'_id':DB.getObjectID(ctx.params.id)});
+    let article = articles[0];    
+    article.comments = article.comments.filter(comment => comment._id !=  ctx.params.commentid)    
+    await DB.update(ArticalCollection, {'_id':DB.getObjectID(ctx.params.id)}, {'comments':article.comments});
+
+    try {
+        if(result.result.ok) {
+            return ctx.redirect('/article/show?id=' + ctx.params.id);
+        }
+    } catch (err) {
+        ctx.body = err;
+        return 
+    }   
 })
 
 function isLoggedIn(ctx, next){
